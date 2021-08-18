e5a95f72ece0dcbf0eb848febdeb290de3d12c8d
Bonaventure DOSSOU <43601208+bonaventuredossou@users.noreply.github.com>
2021-08-18 07:02:33 -0400

    Update k_medoids_better.py

--- k_medoids_better.py
+++ k_medoids_better.py
@@ -62,21 +62,33 @@ if not os.path.exists('distance_matrix_D1.npy'):
 else:
     print('Existing distance matrix --- loading...')
 
-def_distance_matrix = np.load('distance_matrix_D1.npy')
-matrix = def_distance_matrix.tolist()
+if not os.path.exists('medoids_k_{}.npy'.format(k)):
+    def_distance_matrix = np.load('distance_matrix_D1.npy')
+    matrix = def_distance_matrix.tolist()
+    start = time.time()
+    km = kmedoids(matrix, initial_medoids, data_type='distance_matrix')
+    km.process()
+    centers = km.get_medoids()
+    clusters = km.get_clusters()
+    end = time.time()
+    delay = (end - start) / 60
+    print('Time of K-medoid (PAM) execution: {} minutes'.format(delay))
 
-start = time.time()
-km = kmedoids(matrix, initial_medoids, data_type='distance_matrix')
-km.process()
-centers = km.get_medoids()
-clusters = km.get_clusters()
-end = time.time()
-delay = (end - start) / 60
-print('Time of K-medoid (PAM) execution: {} minutes'.format(delay))
+    print(centers)
+    print(clusters)
 
-print(centers)
-print(clusters)
+    np.save('medoids_k_{}'.format(k),
+            centers)  # saves final medoids ids --- we can easily retrieve the sequences later using those ids
+    np.save('clusters_k_{}'.format(k), clusters)  # saves final clusters members ids
+else:
+    medoids = np.load('medoids_k_{}.npy'.format(k), allow_pickle=True)
+    clusters = np.load('clusters_k_{}.npy'.format(k), allow_pickle=True)
 
-np.save('medoids_k_{}'.format(k),
-        centers)  # saves final medoids ids --- we can easily retrieve the sequences later using those ids
-np.save('clusters_k_{}'.format(k), clusters)  # saves final clusters members ids
+    sequences_medoids = dataset["Sequences"][medoids].tolist()
+    print(sequences_medoids)
+
+    # filtering out medoids who appears to be members of their own clusters
+    clusters_sequences = {med: dataset["Sequences"][list(set(members).difference(set(medoids)))].tolist() for
+                          med, members in zip(sequences_medoids,
+                                              clusters.tolist())}
+    print(clusters_sequences)
