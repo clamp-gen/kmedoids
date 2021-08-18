a0bbaa58ceddcb8e61fb212df656a0efe2c436d5
Bonaventure DOSSOU <43601208+bonaventuredossou@users.noreply.github.com>
2021-08-18 12:15:21 -0400

    Update k_medoids_better.py

--- k_medoids_better.py
+++ k_medoids_better.py
@@ -6,8 +6,14 @@ from nltk import edit_distance
 from pyclustering.cluster.kmedoids import kmedoids
 import numpy as np
 import pandas as pd
+import argparse
 
 np.random.seed(1234)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-k", "--n_medoids", default=500, type=int,
+                help="number of medoids (prototypes)")
+args = vars(ap.parse_args())
 
 
 def distance_measuring_(seq1, seq2):
@@ -51,44 +57,61 @@ def get_distance_matrix(samples):
     return dist_mat
 
 
-dataset = pd.read_csv('D1.csv')
-sequences = dataset.Sequences.tolist()
-k = 500
-initial_medoids = _get_init_centers(k, sequences)
-if not os.path.exists('distance_matrix_D1.npy'):
-    print('Building the matrix distance...')
-    distance_matrix = get_distance_matrix(sequences)
-    np.save('distance_matrix', distance_matrix)
-else:
-    print('Existing distance matrix --- loading...')
+def main(args):
+    dataset = pd.read_csv('D1.csv')
+    sequences = dataset.Sequences.tolist()
+    k = args.n_medoids
+    initial_medoids = _get_init_centers(k, sequences)
+    if not os.path.exists('distance_matrix_D1.npy'):
+        print('Building the matrix distance...')
+        distance_matrix = get_distance_matrix(sequences)
+        np.save('distance_matrix', distance_matrix)
+    else:
+        print('Loading existing distance matrix...')
 
-if not os.path.exists('medoids_k_{}.npy'.format(k)):
-    def_distance_matrix = np.load('distance_matrix_D1.npy')
-    matrix = def_distance_matrix.tolist()
-    start = time.time()
-    km = kmedoids(matrix, initial_medoids, data_type='distance_matrix')
-    km.process()
-    centers = km.get_medoids()
-    clusters = km.get_clusters()
-    end = time.time()
-    delay = (end - start) / 60
-    print('Time of K-medoid (PAM) execution: {} minutes'.format(delay))
+    if not os.path.exists('medoids_k_{}.npy'.format(k)):
+        def_distance_matrix = np.load('distance_matrix_D1.npy')
+        matrix = def_distance_matrix.tolist()
+        start = time.time()
+        km = kmedoids(matrix, initial_medoids, data_type='distance_matrix')
+        km.process()
+        centers = km.get_medoids()
+        clusters = km.get_clusters()
+        end = time.time()
+        delay = (end - start) / 60
+        print('Time of K-medoid (PAM) execution: {} minutes'.format(delay))
 
-    print(centers)
-    print(clusters)
+        print(centers)
+        print(clusters)
 
-    np.save('medoids_k_{}'.format(k),
-            centers)  # saves final medoids ids --- we can easily retrieve the sequences later using those ids
-    np.save('clusters_k_{}'.format(k), clusters)  # saves final clusters members ids
-else:
-    medoids = np.load('medoids_k_{}.npy'.format(k), allow_pickle=True)
-    clusters = np.load('clusters_k_{}.npy'.format(k), allow_pickle=True)
+        np.save('medoids_k_{}'.format(k),
+                centers)  # saves final medoids ids --- we can easily retrieve the sequences later using those ids
+        np.save('clusters_k_{}'.format(k), clusters)  # saves final clusters members ids
 
-    sequences_medoids = dataset["Sequences"][medoids].tolist()
-    print(sequences_medoids)
+        sequences_medoids = dataset["Sequences"][centers].tolist()
+        print(sequences_medoids)
 
-    # filtering out medoids who appears to be members of their own clusters
-    clusters_sequences = {med: dataset["Sequences"][list(set(members).difference(set(medoids)))].tolist() for
-                          med, members in zip(sequences_medoids,
-                                              clusters.tolist())}
-    print(clusters_sequences)
+        # filtering out medoids who appears to be members of their own clusters
+        clusters_sequences = {med: dataset["Sequences"][list(set(members).difference(set(centers)))].tolist() for
+                              med, members in zip(sequences_medoids,
+                                                  clusters.tolist())}
+        print(clusters_sequences)
+
+    else:
+        print('Loading existing k-medoids ...')
+        medoids = np.load('medoids_k_{}.npy'.format(k), allow_pickle=True)
+        clusters = np.load('clusters_k_{}.npy'.format(k), allow_pickle=True)
+
+        sequences_medoids = dataset["Sequences"][medoids].tolist()
+        print(sequences_medoids)
+
+        # filtering out medoids who appears to be members of their own clusters
+        clusters_sequences = {med: dataset["Sequences"][list(set(members).difference(set(medoids)))].tolist() for
+                              med, members in zip(sequences_medoids,
+                                                  clusters.tolist())}
+        print(clusters_sequences)
+
+
+if __name__ == '__main__':
+    args = ap.parse_args()
+    main(args)
